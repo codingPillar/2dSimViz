55e81395a5320988d813f98886cba57942b63481
HearthWell <codingpillar@gmail.com>
2023-11-30 05:12:59 -0500

    node structure squeletton

---
+++ node/src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    cout << "WEB 2DVIZ SIMULATION SERVER START" << endl;
+
+    /* OPEN TCP/HTTP SERVER */
+
+    /* PUBLISH ROS TOPICS FOR CMD_VEL AND SENSORS */
+
+    return 0;
+}
