999b5b0d084d11d775024e50c9e1330161be2ece
HearthWell <codingpillar@gmail.com>
2023-12-07 07:01:29 -0500

    changed domain values for sim to better ressemble real life

--- client/src/constants.ts
+++ client/src/constants.ts
@@ -32,7 +32,7 @@ export const EPOCH = 20;
 
 /* MAP RELATED */
 export const DEFAULT_OBSTACLE_COUNT = 4;
-export const CANVAS_TO_MAP_FACTOR = 20;
+export const CANVAS_TO_MAP_FACTOR = 60;
 export const MAP_DOMAIN: number[] = [CANVAS_WIDTH / (2 * CANVAS_TO_MAP_FACTOR), CANVAS_WIDTH / (2 * CANVAS_TO_MAP_FACTOR)];
 export const MAP_IMAGE: number[] = [CANVAS_HEIGHT / (2 * CANVAS_TO_MAP_FACTOR), CANVAS_HEIGHT / (2 * CANVAS_TO_MAP_FACTOR)];
 
